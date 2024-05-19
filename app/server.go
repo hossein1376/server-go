bbc3c99b83675beb8c07f5fb9f23a94508270324
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-05-19 14:35:26 +0330

    Serve requests

--- app/server.go
+++ app/server.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
+)
+
+const (
+	HTTPv1   = "HTTP/1.1"
+	StatusOK = "200 OK"
 )
 
 func main() {
@@ -15,10 +21,26 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error accepting connection:", err.Error())
 	}
+
+	err = serve(conn)
+	if err != nil {
+		fmt.Println("Error writing to connection:", err.Error())
+	}
+}
+
+func serve(c net.Conn) error {
+	msg := strings.Builder{}
+	msg.WriteString(HTTPv1)
+	msg.WriteString(" ")
+	msg.WriteString(StatusOK)
+	msg.WriteString("\r\n\r\n")
+
+	_, err := c.Write([]byte(msg.String()))
+	return err
 }
